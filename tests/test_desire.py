124d273c7ed7872d82c734aa976b24494512fd6e
ZQ-art <62333461+ZQ-art@users.noreply.github.com>
2023-09-15 16:18:12 +0800

    fix test_desire and update desire.yml (#56)

--- tests/test_desire.py
+++ tests/test_desire.py
@@ -89,29 +89,13 @@ class TestDesire(unittest.TestCase):
     def setUp(self):
         self.desire = Desire()
 
-    @unittest.skip('TODO: rewrite')
     def test_third_layer(self):
-        expected_values = [
-            0, 10, 20, 30, 40, 50, 60, 70, 80, 90,
-            100, 110, 120, 130, 140, 150, 160, 170, 180, 190,
-            200, 210, 220, 230, 240, 250, 260, 270, 280, 290
-        ]
-        self.desire.third_layer = [
-            10, 20, 30, 40, 50, 60, 70, 80, 90,
-            100, 110, 120, 130, 140, 150, 160, 170, 180, 190,
-            200, 210, 220, 230, 240, 250, 260, 270, 280, 290
-        ]
-        self.assertEqual(self.desire.third_layer, expected_values)
+        third_layer = self.desire.third_layer
+        self.assertEqual(len(third_layer), 30)
+        self.assertEqual(third_layer[0], 0)
 
     def test_set_item(self):
         index = 1
         expected_value = Decimal(42)
         self.desire[index] = expected_value
         self.assertEqual(self.desire._id2nodes[f"D{index}"]._value, expected_value)
-
-    @unittest.skip('bad test condition')
-    def test_repr(self):
-        self.desire[1] = 10
-        self.desire[3] = 30
-        expected_repr = "{'活着': 10, '吃': 30}"
-        self.assertEqual(repr(self.desire), expected_repr)
