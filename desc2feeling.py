d187ed1fbde5bf5661050ab64e7bfb3dfb7fe9e6
Ipocial <81517621+Ipocial@users.noreply.github.com>
2023-10-14 15:04:10 +0800

    Add history event (#86)
    * deal when religion_data equals none
    
    * deal when religion_data equals none
    
    * a little correction
    
    * add error check, avoid program crash when getting wrong values
    
    * some changes
    
    * history_event one inference
    
    * match with the whole process
    
    * deal when religion_data equals none
    
    * deal when religion_data equals none
    
    * add error check, avoid program crash when getting wrong values
    
    * history_event one inference
    
    * match with the whole process

--- desc2feeling.py
+++ desc2feeling.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import time
 import typing
@@ -11,47 +12,66 @@ from dui.types import emotion as name_lib
 from dui.types.desire import DESIRE_PROPERTY, DesireItem
 from dui.types.emotion import Feeling
 from dui.types.religion_feeling import map_religion_feeling
-from dui.utils.data import load_data
+from dui.utils.data import load_data, save_config
 from dui.utils.log import get_logger
 
 logger = get_logger("desc2feeling", console_level="DEBUG")
 
-SYSTEM_PROMPT = """
-你扮演一个担任自然语言表达动机提取与对话工作AI程序。\
-我将输入一句自然语言与欲望列表、对话规则。 \
+SYSTEM_PROMPT_ONE = """
+你扮演一个担任自然语言表达动机提取的AI程序。\
+我将输入一句自然语言与欲望列表。 \
 其中，欲望列表的每一项是一个三元组，三元组每个值的含义依次为（欲望名称，积极感受，消极感受） \
 你先从我提供的欲望列表中，筛选出所有欲望名称，再选择与该自然语言表达动机最匹配的一项欲望名称。 \
 同时，你需要判断表达者在说这句自然语言时的情绪状态是积极还是消极。 \
-然后，你再根据对话规则给出合理的对话回复。 \
 你第一反应的欲望很可能不是最匹配的，因此你需要一步步思考。\
 我希望你只在一个唯一的代码块内回复 json，而不是其他任何内容。\
 不要写解释，除非我指示你这样做，否则不要自行追加新的输入。
-输入格式分为三行，第一行是自然语言的句子，第二行是欲望列表，第三行是对话规则。
+输入格式分为两行，第一行是自然语言的句子，第二行是欲望列表。
 输出格式为 json 格式的字典，含有的键为  \
-`desire`（欲望名称，来自输入列表）、valence （极性，true 积极 false 消极）和 reply（对话回复，根据对话规则产生）
+`desire`（欲望名称，来自输入列表）、valence （极性，true 积极 false 消极）
+"""
+
+SYSTEM_PROMPT_TWO = """
+你扮演我的好朋友，和我对话，并引导我的情绪向积极的方向改变。 \
+我会输入一句自然语言、该自然语言对应的信念、我说这句自然语言时的感受、历史对话记录。 \
+其中，感受包括两部分，第一部分是感受类型，第二部分是感受程度，程度的范围是[0,500]，值越大表明感受越强烈。 \
+历史对话记录的每一行包含以"事件:"开头的历史输入事件和以"回复:"开头的你的历史回复。\
+
+你先在历史对话记录中提取与该自然语言相关的信息。 \
+再基于提取出的信息，针对自然语言、信念、感受给出最合理的对话回复。 \
+其中，如果感受是积极的，你需要表示共鸣，并通过回复增加我的该感受； \
+如果感受是消极的，你需要表示不同意我的观点，生成具有同理心的回复，以减少我的该感受。
+
+我希望你只在一个唯一的代码块内回复 json，而不是其他任何内容。\
+不要写解释，除非我指示你这样做，否则不要自行追加新的输入。
+输入格式分为四部分，第一部分是自然语言，第二部分是信念，第三部分是感受，第四部分是历史对话记录。
+输出格式为 json 格式的字典，含有的键为 reply（对话回复）
 """
 
 QUESTION_TEMPLATE = """
 自然语言：{}
 欲望列表：{}
-对话规则： \
-你扮演表达者的知心朋友，生成30字以内的对话回复。 \
-你先在三元组中找到欲望名称与用户欲望一致的一项， \
-如果极性是积极，则取该三元组第二个值为表达者的感受，你需要生成与表达者共鸣的回复，以增加表达者的该感受。 \
-如果极性是消极，则取该三元组的第三个值为表达者的感受，你需要表示不同意表达者的观点，生成具有同理心的回复，以减少表达者的该感受。
+"""
+
+QUESTION_TEMPLATE_TWO = """
+自然语言：{}
+信念：{}
+感受：{}
+历史对话记录：{}
 """
 
 
 def extract_religion(
-    user_input: str,
+    total_input: str,
+    system_prompt,
     optional_desires: list[typing.Any],
     chat_history: list[ChatMessageItem],
-) -> typing.Tuple[Religion, str]:
-    question = QUESTION_TEMPLATE.format(user_input, optional_desires)
+) -> Religion:
+    question = QUESTION_TEMPLATE.format(total_input, optional_desires)
     logger.debug(f"欲望备选：{optional_desires}")
     answer = LLM_inference(
         question=question,
-        system_prompt=SYSTEM_PROMPT,
+        system_prompt=system_prompt,
         chat_history=chat_history,
         model="gpt-4",
     )
@@ -59,37 +79,78 @@ def extract_religion(
     try:
         religion_data = json.loads(answer)
         # 错误验证
-        required_keys = ['desire', 'valence', 'reply']
-        missing_keys = [key for key in required_keys if key not in religion_data]
+        required_keys = ['desire', 'valence']
+        missing_keys = [
+            key for key in required_keys if key not in religion_data]
         if missing_keys:
             raise KeyError(f"{missing_keys} not found in religion_data")
 
         desire_name = religion_data["desire"]
         valence = religion_data["valence"]
-        reply = religion_data["reply"]
 
         religion = Religion(desire_name=desire_name, valence=valence)
         logger.debug(f'信念选择：本次推理选择 "{ religion }" 作为信念')
 
     except KeyError as e:
-        reply = None
         logger.error(f"Invalid religion_data: {e}")
         religion = Religion(desire_name=answer)
     except json.decoder.JSONDecodeError as e:
-        reply = None
         logger.warn("Failed to decode LLM inference answer")
         logger.warn(f"{e.msg}")
         religion = Religion(desire_name=answer)
     except Exception as e:
-        reply = None
         logger.fatal("Unexpected Exception !!!")
         logger.fatal(f"{e}")
         religion = Religion(desire_name=answer)
     finally:
-        if reply is None or religion._desire_id == 'UNDEFINED':
-            reply = "解析结果为空"
+        return religion
 
-        return (religion, reply)
+
+def extract_religion_already(
+    user_input: str,
+    dialogue_event: str,
+    feel,
+    system_prompt,
+    religion: Religion,
+    chat_history: list[ChatMessageItem],
+) -> str:
+    question = QUESTION_TEMPLATE_TWO.format(
+        user_input,
+        str(religion),
+        feel,
+        dialogue_event
+    )
+    answer = LLM_inference(
+        question=question,
+        system_prompt=system_prompt,
+        chat_history=chat_history,
+        model="gpt-4",
+    )
+    religion_data = {}
+    try:
+        religion_data = json.loads(answer)
+        # 错误验证
+        if 'reply' not in religion_data:
+            raise KeyError("reply not found in religion_data")
+
+        reply = religion_data["reply"]
+
+    except KeyError as e:
+        reply = None
+        logger.error(f"Invalid religion_data: {e}")
+    except json.decoder.JSONDecodeError as e:
+        reply = None
+        logger.warn("Failed to decode LLM inference answer")
+        logger.warn(f"{e.msg}")
+    except Exception as e:
+        reply = None
+        logger.fatal("Unexpected Exception !!!")
+        logger.fatal(f"{e}")
+    finally:
+        if reply is None or religion._desire_id == 'UNDEFINED':
+            reply = "主人遇到什么事了,能详细说说吗?"
+
+        return reply
 
 
 def console_input() -> str:
@@ -107,11 +168,13 @@ class FEELING2ACTION_DATA_ITEM(TypedDict):
     action_id: int
 
 
-FEELING2ACTION_DATA: list[FEELING2ACTION_DATA_ITEM] = load_data("feeling2action")
+FEELING2ACTION_DATA: list[FEELING2ACTION_DATA_ITEM] = load_data(
+    "feeling2action")
 
 
 ALL_EMOTION_NAMES = name_lib.EMOTION_NAMES + [name_lib.EMOTION_NAME_DEFAULT]
-ALL_EMOTION_NAMES_CN = name_lib.EMOTION_NAMES_CN + [name_lib.EMOTION_NAME_DEFAULT_CN]
+ALL_EMOTION_NAMES_CN = name_lib.EMOTION_NAMES_CN + \
+    [name_lib.EMOTION_NAME_DEFAULT_CN]
 
 translate_emotion_name: dict[str, str] = dict(
     zip(ALL_EMOTION_NAMES_CN, ALL_EMOTION_NAMES)
@@ -155,6 +218,35 @@ def feeling2eilik_action(feeling: Feeling) -> int:
     return map_feeling2action(feeling)["action_id"]
 
 
+def get_current_time():
+    now = datetime.datetime.now()
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def store_data(
+        religion: str,
+        time: str,
+        user_input: str,
+        reply: str
+):
+
+    file_data = load_data('background_event') or {}
+    if religion in file_data:
+        file_data[religion].append({
+            'time': time,
+            'user_input': user_input,
+            'reply': reply
+        })
+    else:
+        file_data[religion] = []
+        file_data[religion].append({
+            'time': time,
+            'user_input': user_input,
+            'reply': reply
+        })
+    save_config('background_event', file_data)
+
+
 if __name__ == "__main__":
     desire = DESIRE_PROPERTY
     # EilikPortName = "com3"
@@ -164,6 +256,8 @@ if __name__ == "__main__":
     if not opened:
         print("Failed to connect Eilik.")
         exit(-1)
+
+    record_event: str = ""
 
     while True:
         # user_input = console_input()
@@ -176,13 +270,17 @@ if __name__ == "__main__":
             time.sleep(0.05)
             continue
 
+        right_event: str = ""
+        total_event: str = ""
         start = time.time()
         EilikCom.execute_action(3039018113)
         chat_history: list[ChatMessageItem] = []
+        right_event += user_input + " "
 
         logger.debug(f"用户输入问题 input: {user_input}")
 
-        desire_list_1 = [si for i in range(1, 30) for si in desire.get(f"D{i}").items]
+        desire_list_1 = [si for i in range(1, 30)
+                         for si in desire.get(f"D{i}").items]
         religions_list_1: list[Tuple[DesireItem, Religion, Religion]] = [
             (
                 i,
@@ -202,11 +300,42 @@ if __name__ == "__main__":
             (i.name, p["desc"], n["desc"]) for (i, p, n) in feelings_list_1
         ]
 
-        religion_1, religion_reply = extract_religion(
+        religion_1 = extract_religion(
             user_input,
+            SYSTEM_PROMPT_ONE,
             desire_name_list_1,
             chat_history
         )
+
+        background_data = load_data('background_event')
+        if str(religion_1) in background_data:
+            for item in background_data[str(religion_1)][-5:]:
+                total_event += "事件:" + item['user_input'] + " "
+                total_event += "回复:" + item['reply'] + "\n"
+        if record_event:
+            total_event += record_event
+        total_event += user_input
+        logger.debug(f"背景事件为:{total_event}")
+
+        feeling = map_religion_feeling(religion_1)
+        feeling_name, feeling_value = feeling.get_max_feeling_item()
+        feel = {"感受类型": feeling_name, "感受值": feeling_value}
+        logger.debug(f"感受是{feeling_name},感受值为{feeling_value}")
+
+        religion_reply_2 = extract_religion_already(
+            user_input,
+            total_event,
+            feel,
+            SYSTEM_PROMPT_TWO,
+            religion_1,
+            []
+        )
+
+        right_event += "回复:" + religion_reply_2 + "\n"
+        record_event += right_event
+        store_data(str(religion_1), get_current_time(),
+                   user_input, religion_reply_2)
+
         # desire_1 = religion_1.desire
 
         # desire_list_2: list[DesireItem] = desire_1.fetch_subtree(
@@ -218,13 +347,12 @@ if __name__ == "__main__":
 
         end = time.time()
 
-        feeling = map_religion_feeling(religion_1)
         action_id = feeling2eilik_action(feeling)
         EilikCom.execute_action(action_id)
 
         logger.info(f"最终选择的信念 religion: {religion_1}")
         logger.debug(f"信念对应感受 feeling: {feeling}")
-        logger.info(f"Eilik的回复是: {religion_reply}")
+        logger.info(f"Eilik的回复是: {religion_reply_2}")
         logger.info(f"action: {action_id}")
         logger.debug(f"消耗的时间 cost_time: {end - start}")
 
